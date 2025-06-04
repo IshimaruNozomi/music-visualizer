a3365e1d703b28f63882c54b1fbc6ffd27f1cf36
Nozomi Ishimaru <s2322096@stu.musashino-u.ac.jp>
2025-06-04 17:24:02 +0900

    変更

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/music-visualizer/', // GitHubのリポジトリ名に合わせる
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
